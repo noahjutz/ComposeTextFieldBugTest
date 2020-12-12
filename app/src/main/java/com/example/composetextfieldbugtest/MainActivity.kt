37ba92a2ec23c768aecf4a0801eb450ab3a0a9fd
Noah Jutz <noahjutz@tutanota.de>
2020-12-12 13:52:50 +0100

    Add content

--- app/src/main/java/com/example/composetextfieldbugtest/MainActivity.kt
+++ app/src/main/java/com/example/composetextfieldbugtest/MainActivity.kt
@@ -2,20 +2,53 @@ package com.example.composetextfieldbugtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                Text("Hello World!")
+                Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    var textFieldValue by remember { mutableStateOf("") }
+                    MyTextField(
+                        Modifier
+                            .clip(RoundedCornerShape(4.dp))
+                            .background(MaterialTheme.colors.onSurface.copy(alpha = 0.12f))
+                            .padding(8.dp),
+                        value = textFieldValue,
+                        onValueChange = { textFieldValue = it }
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun MyTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    BasicTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = { onValueChange(it) },
+    )
 }
