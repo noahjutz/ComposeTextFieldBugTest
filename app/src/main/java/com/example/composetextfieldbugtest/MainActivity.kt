04a17744d7af110898a5ded1df1a226862ffa4b7
Noah Jutz <noahjutz@tutanota.de>
2020-12-12 13:39:38 +0100

    Remove empty project code

--- app/src/main/java/com/example/composetextfieldbugtest/MainActivity.kt
+++ app/src/main/java/com/example/composetextfieldbugtest/MainActivity.kt
@@ -3,36 +3,19 @@ package com.example.composetextfieldbugtest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composetextfieldbugtest.ui.ComposeTextFieldBugTestTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTextFieldBugTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
+                Text("Hello World!")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTextFieldBugTestTheme {
-        Greeting("Android")
     }
 }
